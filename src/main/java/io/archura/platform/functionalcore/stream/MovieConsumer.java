7a1aab9771a04df6aeb267f7af4157eb48516f25
Can MOGOL <canmogol@gmail.com>
2022-10-11 22:10:37 +0200

    Remove spring dependency.

--- src/main/java/io/archura/platform/functionalcore/stream/MovieConsumer.java
+++ src/main/java/io/archura/platform/functionalcore/stream/MovieConsumer.java
@@ -1,13 +1,11 @@
 package io.archura.platform.functionalcore.stream;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.archura.platform.api.context.Context;
 import io.archura.platform.api.logger.Logger;
 import io.archura.platform.api.type.Configurable;
 import io.archura.platform.api.type.functionalcore.StreamConsumer;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 import java.util.Map;
 
 @Component
@@ -21,15 +19,12 @@ public class MovieConsumer implements StreamConsumer, Configurable {
     }
 
     @Override
-    public void consume(Context context, byte[] key, byte[] value) {
-        try {
-            final Logger logger = context.getLogger();
-            final ObjectMapper objectMapper = context.getObjectMapper();
-            final Movie movie = objectMapper.readValue(value, Movie.class);
-            logger.info("key: %s, value: %s", new String(key), movie);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void consume(Context context, String key, Map<String, String> value) {
+        final Logger logger = context.getLogger();
+        logger.info("configuration: %s", configuration);
+
+        final Movie movie = new Movie(value.get("title"), Integer.parseInt(value.get("year")));
+        logger.info("key: %s, value: %s", key, movie);
     }
 
 }
